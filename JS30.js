ff07394c99625f0124b3f2b3a421a83c09895890
Bhati-Akash <akashbhati67@gmail.com>
2024-09-24 21:42:11 +0530

    counter 2
    counter 2 from JS 30 days

--- JS30.js
+++ JS30.js
@@ -9,3 +9,24 @@ let x =counter(2)();
 let y =counter(3)();
 let z =counter(4)();
 console.log(x, y,z);
+
+// 2665. Counter II 
+
+function createCounter(init) {
+ let counterCount = init;
+    return {
+        increment: function increment() {
+            return ++counterCount
+        },
+        decrement: function decrement() {
+            return --counterCount
+        },
+        reset: function reset() {
+            return counterCount = init
+        }
+    }
+}
+var b = createCounter(50)
+console.log(b.increment());
+console.log(b.reset());
+console.log(b.decrement());
