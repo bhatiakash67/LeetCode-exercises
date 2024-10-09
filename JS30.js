3b5625385dcbfb5cefd4dd8eb7fc12615a72b649
Bhati-Akash <akashbhati67@gmail.com>
2024-10-10 02:41:03 +0530

    spread, rest operators

--- JS30.js
+++ JS30.js
@@ -1,5 +1,4 @@
 // 2620.
-
 // function counter(n) {
 //     return function() {
 //         return n++
@@ -11,7 +10,6 @@
 // console.log(x, y,z);
 
 // 2665. Counter II 
-
 // function createCounter(init) {
 //  let counterCount = init;
 //     return {
@@ -33,10 +31,48 @@
 
 // 2635. Apply Transform Over Each Element in Array 
 // callback function use case
-var map = function(arr, fn) {
-    const transformedArr = [];
-    for (let i = 0; i < arr.length; i++) {
-      transformedArr[i] = fn(arr[i], i);
-    }
-    return transformedArr;
-  };
+// var map = function (arr, fn) {
+//   const transformedArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     transformedArr[i] = fn(arr[i], i);
+//   }
+//   return transformedArr;
+// };
+
+// 2634. Filter Elements from array
+// function add(n) {
+//   let newArr = []
+//   for (let i = 0; i < n.length; i++) {
+//     if (i > 2) {
+//       newArr.push(n[i])
+//     }
+//   }
+//   return newArr
+// }
+// let a = [1, 2, 3, 4, 5]
+// console.log(add(a));
+
+// function filter(arr, fn) {
+//   let updatedArr = []
+//   for (let i = 0; i < arr.length; i++) {
+//     if (fn(n[i], i)) {
+//       updatedArr.push(arr[i])
+//     }
+//   }
+//   return updatedArr
+// }
+
+// 2626. Array reduce transformation
+
+function reduce(nums, fn, init) {
+  let newVal = init
+  for (let i = 0; i < nums.length; i++) {
+    newVal = fn(newVal, nums[i])
+  }
+  return newVal
+}
+
+let a = [1, 2, 3, 4, 5, 6]
+let result = a.reduce((acc, curr) => acc + curr)
+console.log(result);
+
--- LeetCode.js
+++ LeetCode.js
@@ -1,8 +1,31 @@
 // 2619. Array prototype last
 // method definition, this keyword in a method
-let b = [1,2,3,4,5,6]
-b.lastArr = function(){
-    return this[this.length-1]
+// let b = [1, 2, 3, 4, 5, 6]
+// b.lastArr = function () {
+//     return this[this.length - 1]
+// }
+// let a = b.lastArr()
+// console.log(a);
+
+
+// Arguments object - 
+function a() {
+    return console.log(arguments.length);
 }
-let a =b.lastArr()
-console.log(a);
+a(1, 2, 3, 4, 5)
+
+// rest operator 
+function myFun(...manyMoreArgs) {
+    // console.log("a", a);
+    // console.log("b", b);
+    console.log("manyMoreArgs", manyMoreArgs);
+}
+
+myFun("one", "two", "three", "four", "five", "six");
+
+// Spread operator 
+const numbers = [1, 3, 5, 7, 10, 200, 90, 59];
+function addNumbers(a, b, c, d) {
+  return a + b + c + d;
+}
+console.log(addNumbers(...numbers));
